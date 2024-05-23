9aa3bce800bc3294d51863076d21633de098fc64
heavyn1 <bunedusupov968@gmail.com>
2024-05-23 15:21:35 +0500

    first commit

---
+++ maun.js
@@ -0,0 +1,11 @@
+let name = prompt("Ismigizni kiriting")
+let login = prompt("Spase logini kiriting")
+let password = prompt("Spase password kiriting")
+
+const Maluot = {
+    login: login,
+    name: name,
+
+    password: password
+}
+console.log(Maluot);
